187517eeb79c317df0c41a1e3d8f721a4fb32759
webdevaminul <webdev.aminul@gmail.com>
2024-08-09 15:58:25 +0600

    Connected to Mongodb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
