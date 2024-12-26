617ab3e1fe2bd1d1ca5a2629bc57effd533bb272
webdevaminul <webdev.aminul@gmail.com>
2024-12-26 22:54:52 +0600

    Added confirm message in root

--- backend/index.js
+++ backend/index.js
@@ -50,6 +50,11 @@ app.use((err, req, res, next) => {
   return res.status(statusCode).json({ success: false, statusCode, message });
 });
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("WDA Real Estate server is running fine");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
